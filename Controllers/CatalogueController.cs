1c72b9accaac5c6193d1d01ca7e9fce7e7faec51
TSD <cameleon@192.168.1.4>
2025-12-21 15:11:29 +0000

    Commit 7: Développement de l'interface catalogue et système de panier

---
+++ Controllers/CatalogueController.cs
@@ -0,0 +1,83 @@
+using Microsoft.AspNetCore.Mvc;
+using BrasilBurger.Web.Service;
+using BrasilBurger.Web.ViewModels;
+
+namespace BrasilBurger.Web.Controllers
+{
+    public class CatalogueController : Controller
+    {
+        private readonly IBurgerService _burgerService;
+        private readonly IMenuService _menuService;
+        private readonly IComplementService _complementService;
+
+        public CatalogueController(
+            IBurgerService burgerService,
+            IMenuService menuService,
+            IComplementService complementService)
+        {
+            _burgerService = burgerService;
+            _menuService = menuService;
+            _complementService = complementService;
+        }
+
+        // GET: /Catalogue ou /
+        public async Task<IActionResult> Index(string? filtre)
+        {
+            var burgers = await _burgerService.ListerParEtatAsync("disponible");
+            var menus = await _menuService.ListerParEtatAsync("disponible");
+
+            var viewModel = new CatalogueViewModel
+            {
+                Burgers = burgers,
+                Menus = menus,
+                Filtre = filtre
+            };
+
+            return View(viewModel);
+        }
+
+        // GET: /Catalogue/DetailsBurger/5
+        public async Task<IActionResult> DetailsBurger(int id)
+        {
+            var burger = await _burgerService.TrouverParIdAsync(id);
+            if (burger == null)
+            {
+                return NotFound();
+            }
+
+            var complements = await _complementService.ListerParEtatAsync("disponible");
+
+            var viewModel = new DetailsViewModel
+            {
+                Burger = burger,
+                ComplementsDisponibles = complements
+            };
+
+            return View(viewModel);
+        }
+
+        // GET: /Catalogue/DetailsMenu/5
+        public async Task<IActionResult> DetailsMenu(int id)
+        {
+            var menu = await _menuService.TrouverParIdAsync(id);
+            if (menu == null)
+            {
+                return NotFound();
+            }
+
+            var viewModel = new DetailsViewModel
+            {
+                Menu = menu
+            };
+
+            return View(viewModel);
+        }
+
+        // Méthode pour récupérer le nombre d'items dans le panier (pour le badge)
+        public IActionResult GetPanierCount()
+        {
+            var panier = Helpers.PanierHelper.GetPanier(HttpContext.Session);
+            return Json(new { count = panier.Sum(i => i.Quantite) });
+        }
+    }
+}
