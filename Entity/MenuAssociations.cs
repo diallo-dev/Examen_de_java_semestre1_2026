145b22fa63e12c4b2908d14651245bad82a9f450
Moustapha <moustapha@192.168.1.7>
2025-12-22 15:19:15 +0000

    fix: correction des noms de propriétés dans MenuBurger et MenuComplement

--- Entity/MenuAssociations.cs
+++ Entity/MenuAssociations.cs
@@ -1,18 +1,37 @@
+
+using System.ComponentModel.DataAnnotations.Schema;
+
 namespace BrasilBurger.Web.Entity
 {
+    [Table("menu_burger")]
     public class MenuBurger
     {
+        [Column("id")]
         public int Id { get; set; }
-        public int IdMenu { get; set; }
-        public int IdBurger { get; set; }
+        
+        [Column("menu_id")]
+        public int MenuId { get; set; }
+        
+        [Column("burger_id")]
+        public int BurgerId { get; set; }
+        
+        [Column("quantite")]
         public int Quantite { get; set; }
     }
 
+    [Table("menu_complement")]
     public class MenuComplement
     {
+        [Column("id")]
         public int Id { get; set; }
-        public int IdMenu { get; set; }
-        public int IdComplement { get; set; }
+        
+        [Column("menu_id")]
+        public int MenuId { get; set; }
+        
+        [Column("complement_id")]
+        public int ComplementId { get; set; }
+        
+        [Column("quantite")]
         public int Quantite { get; set; }
     }
 }
