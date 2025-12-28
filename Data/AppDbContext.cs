00f135602caca697491b52560bc8392118f738df
Moustapha <moustapha@192.168.1.4>
2025-12-28 15:55:44 +0000

    modification de appcontext

--- Data/AppDbContext.cs
+++ Data/AppDbContext.cs
@@ -24,19 +24,7 @@ namespace BrasilBurger.Web.Data
         {
             base.OnModelCreating(modelBuilder);
 
-            
-            foreach (var entity in modelBuilder.Model.GetEntityTypes())
-            {
-                entity.SetTableName(entity.GetTableName().ToLower());
-                foreach (var property in entity.GetProperties())
-                {
-                    property.SetColumnName(property.Name.ToLower());
-                }
-            }
-
-            
-            
-            
+            // 1. BURGER
             modelBuilder.Entity<Burger>(entity =>
             {
                 entity.ToTable("burger");
@@ -44,14 +32,14 @@ namespace BrasilBurger.Web.Data
                 entity.Property(e => e.EtatStock).HasColumnName("etatstock");
             });
 
-            
+            // 2. COMPLEMENT
             modelBuilder.Entity<Complement>(entity =>
             {
                 entity.ToTable("complement");
                 entity.Property(e => e.UrlImage).HasColumnName("url_image");
             });
 
-            
+            // 3. MENU
             modelBuilder.Entity<Menu>(entity =>
             {
                 entity.ToTable("menu");
@@ -61,63 +49,65 @@ namespace BrasilBurger.Web.Data
                 entity.Ignore(e => e.Complements);
             });
 
-            
-                                modelBuilder.Entity<Client>(entity =>
-                {
-                    entity.ToTable("client");
-                    entity.HasKey(e => e.Id);
-                    entity.Property(e => e.Id).HasColumnName("id_client"); 
-                    entity.Property(e => e.Nom).HasColumnName("nom");
-                    entity.Property(e => e.Prenom).HasColumnName("prenom");    
-                    entity.Property(e => e.Adresse).HasColumnName("adresse");
-                    entity.Property(e => e.Type).HasColumnName("type"); 
-                    entity.Property(e => e.Email).HasColumnName("email"); 
-                    entity.Property(e => e.MotDePasse).HasColumnName("mot_de_passe");
-                    entity.Property(e => e.Telephone).HasColumnName("telephone");
-                });
+            // 4. CLIENT
+            modelBuilder.Entity<Client>(entity =>
+            {
+                entity.ToTable("client");
+                entity.HasKey(e => e.Id);
+                entity.Property(e => e.Id).HasColumnName("id_client"); 
+                entity.Property(e => e.MotDePasse).HasColumnName("mot_de_passe");
+            });
 
-           
-           
+            // 5. COMMANDE (Correction ID_ZONE, ID_CLIENT, ID_GESTIONNAIRE)
             modelBuilder.Entity<Commande>(entity =>
-                {
-                    entity.ToTable("commande");
-                    entity.HasKey(e => e.Id);
+            {
+                entity.ToTable("commande");
+                entity.HasKey(e => e.Id);
+                entity.Property(e => e.Id).HasColumnName("id"); 
+                entity.Property(e => e.IdZone).HasColumnName("id_zone");
+                entity.Property(e => e.Date).HasColumnName("date");
+                entity.Property(e => e.FraisLivraison).HasColumnName("frais_livraison");
+                entity.Property(e => e.MontantTotal).HasColumnName("montant_total");
+                entity.Property(e => e.IdGestionnaire).HasColumnName("id_gestionnaire");
+                entity.Property(e => e.IdClient).HasColumnName("id_client");
+                entity.Property(e => e.IdLivreur).HasColumnName("id_livreur");
+                entity.Property(e => e.EtatCmd).HasColumnName("etat_cmd");
+                entity.Property(e => e.LieuConsommation).HasColumnName("lieu_consommation");
 
-                    // MAPPING COMPLET DES COLONNES (D'après ton export Neon)
-                    entity.Property(e => e.Id).HasColumnName("id"); 
-                    entity.Property(e => e.IdZone).HasColumnName("id_zone"); // <-- LA CORRECTION POUR L'ERREUR ACTUELLE
-                    entity.Property(e => e.Date).HasColumnName("date");
-                    entity.Property(e => e.FraisLivraison).HasColumnName("frais_livraison");
-                    entity.Property(e => e.MontantTotal).HasColumnName("montant_total");
-                    entity.Property(e => e.IdGestionnaire).HasColumnName("id_gestionnaire");
-                    entity.Property(e => e.IdClient).HasColumnName("id_client");
-                    entity.Property(e => e.IdLivreur).HasColumnName("id_livreur");
-                    entity.Property(e => e.EtatCmd).HasColumnName("etat_cmd");
-                    entity.Property(e => e.LieuConsommation).HasColumnName("lieu_consommation");
+                entity.HasOne(e => e.Client).WithMany().HasForeignKey(e => e.IdClient);
+                entity.Ignore(e => e.CommandeBurgers);
+                entity.Ignore(e => e.CommandeMenus);
+            });
 
-                    // Relations
-                    entity.HasOne(e => e.Client).WithMany().HasForeignKey(e => e.IdClient);
-                    
-                    // On ignore les listes pour éviter les cycles
-                    entity.Ignore(e => e.CommandeBurgers);
-                    entity.Ignore(e => e.CommandeMenus);
-                });
-          
-          
-           modelBuilder.Entity<CommandeBurger>(entity =>
-                {
-                    entity.ToTable("commande_burger");
-                    entity.Property(e => e.IdCommande).HasColumnName("id_commande");
-                    entity.Property(e => e.BurgerId).HasColumnName("id_burger");
-                    entity.Property(e => e.Quantite).HasColumnName("quantite");
-                    
-                    // LA CORRECTION POUR L'ERREUR ACTUELLE :
-                    entity.Property(e => e.PrixUnitaire).HasColumnName("prix_unitaire"); 
-                    
-                    entity.Ignore(e => e.Burger);
-                });
-            
-            
+            // 6. COMMANDE_BURGER (Correction PRIX_UNITAIRE)
+            modelBuilder.Entity<CommandeBurger>(entity =>
+            {
+                entity.ToTable("commande_burger");
+                entity.Property(e => e.IdCommande).HasColumnName("id_commande");
+                entity.Property(e => e.BurgerId).HasColumnName("id_burger");
+                entity.Property(e => e.PrixUnitaire).HasColumnName("prix_unitaire");
+                entity.Ignore(e => e.Burger);
+            });
+
+            // 7. COMMANDE_MENU (Correction PRIX_UNITAIRE)
+            modelBuilder.Entity<CommandeMenu>(entity =>
+            {
+                entity.ToTable("commande_menu");
+                entity.Property(e => e.IdCommande).HasColumnName("id_commande");
+                entity.Property(e => e.MenuId).HasColumnName("id_menu");
+                entity.Property(e => e.PrixUnitaire).HasColumnName("prix_unitaire");
+            });
+
+            // 8. PAIEMENT
+            modelBuilder.Entity<Paiement>(entity =>
+            {
+                entity.ToTable("paiement"); 
+                entity.HasKey(e => e.Id);
+                entity.Property(e => e.IdCommande).HasColumnName("id_commande");
+                entity.HasOne(p => p.Commande).WithMany().HasForeignKey(p => p.IdCommande);
+            });
+
+            // 9. RELATIONS MENU
             modelBuilder.Entity<MenuBurger>(entity => {
                 entity.ToTable("menu_burger");
                 entity.Property(e => e.MenuId).HasColumnName("id_menu");
@@ -129,39 +119,6 @@ namespace BrasilBurger.Web.Data
                 entity.Property(e => e.MenuId).HasColumnName("id_menu");
                 entity.Property(e => e.ComplementId).HasColumnName("id_complement");
             });
-
-
-
-
-             modelBuilder.Entity<CommandeMenu>(entity =>
-                    {
-                        entity.ToTable("commande_menu");
-                        entity.Property(e => e.IdCommande).HasColumnName("id_commande");
-                        entity.Property(e => e.MenuId).HasColumnName("id_menu");
-                        entity.Property(e => e.Quantite).HasColumnName("quantite");
-                        
-                        // On met l'underscore par précaution ici aussi
-                        entity.Property(e => e.PrixUnitaire).HasColumnName("prix_unitaire"); 
-                    });
-
-
-
-                            modelBuilder.Entity<Paiement>(entity =>
-                {
-                    entity.ToTable("paiement"); 
-                    entity.HasKey(e => e.Id);
-                    
-                    entity.Property(e => e.Id).HasColumnName("id");
-                    entity.Property(e => e.Date).HasColumnName("date");
-                    entity.Property(e => e.Montant).HasColumnName("montant");
-                    entity.Property(e => e.Mode).HasColumnName("mode");
-                    entity.Property(e => e.IdCommande).HasColumnName("id_commande");
-
-                  
-                    entity.HasOne(p => p.Commande)
-                        .WithMany()
-                        .HasForeignKey(p => p.IdCommande);
-                });
         }
     }
 }
